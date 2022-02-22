b765759cc55d7312fce6838a88932e8b82548608
jinyongp <dev.jinyongp@gmail.com>
2022-02-22 17:57:13 +0900

    Initialize react project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    bundle: ['./src/index'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  devServer: {
+    open: true,
+    port: 4000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules',
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
