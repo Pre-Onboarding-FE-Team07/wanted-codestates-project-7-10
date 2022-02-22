8709781fabcd6f631991adc0a0b16460edc653e2
from97 <skm1104@daum.net>
2022-02-22 20:35:32 +0900

    :art: Use svg as a React Component
    SearchArea 컴포넌트 안에 하드코딩되어 있던 svg를 파일로 분리하고 React Component로 import해서 사용.

--- src/component/SearchArea/SearchArea.jsx
+++ src/component/SearchArea/SearchArea.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchAreaStyled, { Title, SearchBar } from './SearchArea.styles.jsx';
+import SearchIcon from '../../assets/icon_search.svg';
 
 const SearchArea = () => {
   return (
@@ -11,17 +12,7 @@ const SearchArea = () => {
       </Title>
       <SearchBar>
         <SearchBar.Box>
-          <svg
-            width="16"
-            height="16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z"
-              fill="#32383E"
-            ></path>
-          </svg>
+          <SearchIcon />
           <input type="text" placeholder="질환명을 입력해 주세요. " />
         </SearchBar.Box>
         <SearchBar.Button type="button">검색</SearchBar.Button>
