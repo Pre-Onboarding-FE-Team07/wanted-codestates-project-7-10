78bb47143478df9d07e63fbd274d9a1558608dbf
from97 <skm1104@daum.net>
2022-02-22 23:30:10 +0900

    :lipstick: Set webfont 'SpoaqHanSans' as global font

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ export default function App() {
 }
 
 const GlobalStyle = createGlobalStyle`
+  @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
   * {
     padding: 0;
     margin: 0;
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
   }
   body {
     background-color: #cae9ff;
