7ae7fdf87b3770153038dbb32c10ab0371af26de
Lee-Ye-Ji <dpwl30559@gmail.com>
2022-02-23 14:34:15 +0900

    📦️ Add Package download & error confirm

--- webpack.config.js
+++ webpack.config.js
@@ -21,19 +21,23 @@ module.exports = {
   module: {
     rules: [
       {
-        "presets": [
-          [
-            "@babel/preset-env",
-            {
-              "targets": {
-                "esmodules": true
-              }
-            }
+        test: /\.jsx?$/,
+        exclude: '/node_modules',
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  esmodules: true,
+                },
+              },
+            ],
+            '@babel/preset-react',
           ],
-          "@babel/preset-react"
-        ]
-      }
-      
+        },
+      },
     ],
   },
   plugins: [
