4f3e4fe55c6f76a1061e2dfa3720ef2fdc16a309
Lela12 <dbcocotn4@hanmail.net>
2022-02-23 13:56:56 +0900

    :art: fix: index.jsx checkout 오류로 인한 커밋

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
